88fafef87cdfa845339b77b6029393008bf1d4dd
Nicolas Renet <nprenet@gmail.com>
2024-07-29 00:17:41 +0200

    Adding a default model.

--- bin/ddp_htr.py
+++ bin/ddp_htr.py
@@ -13,15 +13,16 @@ import re
 import glob
 from PIL import Image
 
-
-sys.path.append( str( Path(__file__).parents[1] ) )
+root = str( Path(__file__).parents[1] ) 
+sys.path.append( root ) 
 
 import seglib
 
 p = {
 
     "appname": "htr",
-    "model_path": Path.home().joinpath('tmp','models','htr', 'Tridis_by_Torres-Aguilar_2024', 'Tridis_Medieval_EarlyModern.mlmodel'),
+    #"model_path": Path.home().joinpath('tmp','models','htr', 'Tridis_by_Torres-Aguilar_2024', 'Tridis_Medieval_EarlyModern.mlmodel'),
+    "model_path": Path( root, 'models', 'htr', 'default.mlmodel' ),
     "img_paths": set(glob.glob( str(Path.home().joinpath("tmp/data/1000CV/SK-SNA/f5dc4a3628ccd5307b8e97f02d9ff12a/89ce0542679f64d462a73f7e468ae812/*.jpg")))),
     "segmentation_dir": "", # for testing purpose
     "segmentation_file_suffix": "lines.pred", # under each image dir, suffix of the subfolder that contains the segmentation data 
@@ -62,18 +63,20 @@ if __name__ == "__main__":
         if segmentation_object is None:
             print("Could not find a proper segmentation data file. Aborting.")
             sys.exit()
-        else:
-            print("Create segmentation record {} from segmentation data {}".format( segmentation_object, segmentation_file_path ))
-            
+        #else:
+        #    print("Create segmentation record {} from segmentation data {}".format( segmentation_object, segmentation_file_path ))
+         
 
         # 2. HTR inference
+        line_ids =  [ line.id for line in segmentation_object.lines ]
+
         htr_model = models.load_any( args.model_path )
         pred_it = rpred.rpred( 
                         network = htr_model, 
                         im = Image.open( path ), 
                         bounds = segmentation_object )
 
-        for record in pred_it:
-            print(record)
+        for line, line_id, record in zip(range(1, len(line_ids)+1), line_ids, pred_it):
+            print(f'{line}\t{line_id}\t{record}')
 
 
