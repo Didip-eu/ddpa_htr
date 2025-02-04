da7140fbd10f067224b58203ff8be31aa16bc5ed
nicolas <nprenet@gmail.com>
2025-02-04 23:58:46 +0100

    Updating charters_htr.py.

--- libs/charters_htr.py
+++ libs/charters_htr.py
@@ -150,7 +150,7 @@ class ChartersDataset(VisionDataset):
                                     "\n\t + a valid resource dictionary (cf. 'dataset_resource' class attribute)" +
                                     "\n\t + one of the following options: -from_page_xml_dir, -from_work_folder, -from_line_tsv_file")
 
-        trf = v2.Compose( [v2.PILToTensor(), v2.ToDtype(torch.float32, scale=True) ]) 
+        trf = v2.Compose( [v2.PILToTensor()]) #, v2.ToDtype(torch.float32, scale=True) ]) 
         if channel_func is not None:
             trf =  v2.Compose( [trf, AddChannel()] )
         if transform:
@@ -222,11 +222,13 @@ class ChartersDataset(VisionDataset):
         if self.data and not from_line_tsv_file:
             # Generate a TSV file with one entry per img/transcription pair
             self.dump_data_to_tsv(self.data, Path(self.work_folder_path.joinpath(f"charters_ds_{subset}.tsv")) )
-            channel_function_def = None
-            if type(channel_func) is functools.partial:
-                channel_function_def = inspect.getsource(channel_func.func) + str( channel_func.keywords)
-            else:
-                channel_function_def = inspect.getsource( channel_func )
+
+            channel_function_def = ''
+            if channel_func is not None:
+                if type(channel_func) is functools.partial:
+                    channel_function_def = inspect.getsource(channel_func.func) + str( channel_func.keywords)
+                else:
+                    channel_function_def = inspect.getsource( channel_func )
 
             self._generate_readme("README.md", 
                     { 'subset': subset,
@@ -366,7 +368,7 @@ class ChartersDataset(VisionDataset):
 
         Args:
             work_folder_path (Union[Path,str]): a folder containing images (`*.png`),
-            transcription files (`*.gt.txt`) and optional binary polygon masks ('*.mask.npy.gz')
+            transcription files (`*.gt.txt`) and optional extra channel ('*.channel.npy.gz')
 
         Returns:
             List[dict]: a list of samples.
@@ -393,9 +395,9 @@ class ChartersDataset(VisionDataset):
                     sample['transcription']=transcription
 
             # optional mask
-            mask_file_path = img_file_path.with_suffix('.mask.npy.gz')
-            if mask_file_path.exists():
-                sample['img_mask']=mask_file_path
+            channel_file_path = img_file_path.with_suffix('.channel.npy.gz')
+            if channel_file_path.exists():
+                sample['img_channel']=channel_file_path
 
             samples.append( sample )
 
@@ -420,7 +422,7 @@ class ChartersDataset(VisionDataset):
              'transcription': <transcription text>,
              'height': <original height>,
              'width': <original width>,
-            ['img_mask': <2D binary mask> ]
+            ['img_channel': <2D extra channel> ]
             }
 
         """
@@ -435,7 +437,7 @@ class ChartersDataset(VisionDataset):
             img_path, file_or_text, height, width = first_line.split('\t')[:4]
             inline_transcription = False if Path(file_or_text).exists() else True
             # - Is there a mask field?
-            has_mask = len(first_line.split('\t')) > 4
+            has_channel = len(first_line.split('\t')) > 4
             infile.seek(0)
 
             # Note: polygons are not read
@@ -453,8 +455,8 @@ class ChartersDataset(VisionDataset):
 
                 spl = { 'img': work_folder_path.joinpath( img_file ), 'transcription': gt_field,
                         'height': int(height), 'width': int(width) }
-                if has_mask:
-                    spl['img_mask']=work_folder_path.joinpath(fields[4])
+                if has_channel:
+                    spl['img_channel']=work_folder_path.joinpath(fields[4])
                 if expansion_masks and expansion_masks_match is not None:
                     spl['expansion_masks']=eval( expansion_masks_match.group(2))
                 samples.append( spl )
@@ -595,12 +597,12 @@ class ChartersDataset(VisionDataset):
                             Image.fromarray(img_hwc).save( Path( sample['img'] ))
                         # construct an additional, flat channel
                         if config['channel_func'] is not None:
-                            img_mask_hw = config['channel_func']( img_hwc, boolean_mask)
-                            #sample['img_mask']=img_path_prefix.with_suffix('.mask.npy.gz')
-                            sample['img_mask']=img_path_prefix.with_suffix('.mask.npy')
-                            #with gzip.GzipFile(sample['img_mask'], 'w') as zf:
-                            #    np.save( zf, img_mask_hw ) 
-                            np.save( sample['img_mask'], img_mask_hw)
+                            img_channel_hw = config['channel_func']( img_hwc, boolean_mask)
+                            sample['img_channel']=img_path_prefix.with_suffix('.channel.npy.gz')
+                            #sample['img_channel']=img_path_prefix.with_suffix('.channel.npy')
+                            with gzip.GzipFile(sample['img_channel'], 'w') as zf:
+                                np.save( zf, img_channel_hw ) 
+                            #np.save( sample['img_channel'], img_channel_hw)
 
                     with open( img_path_prefix.with_suffix('.gt.txt'), 'w') as gt_file:
                         gt_file.write( sample['transcription'])
@@ -641,8 +643,8 @@ class ChartersDataset(VisionDataset):
                 of.write( '{}\t{}\t{}\t{}'.format( img_path,
                                              gt if not all_path_style else Path(img_path).with_suffix('.gt.txt'),
                                              int(height), int(width) ))
-                if 'img_mask' in sample and sample['img_mask'] is not None:
-                    of.write('\t{}'.format( sample['img_mask'].name ))
+                if 'img_channel' in sample and sample['img_channel'] is not None:
+                    of.write('\t{}'.format( sample['img_channel'].name ))
                 of.write('\n')
                                             
 
@@ -764,18 +766,18 @@ class ChartersDataset(VisionDataset):
         sample['transcription']=self.target_transform( sample['transcription'] )
         sample['img'] = Image.open( img_path, 'r')
 
-        # optional mask is concatenated to the image tensor by the transform
+        # optional channel is concatenated to the image tensor by the transform
         # in an ulterior step - it needs to be made into a Tensor here, because the v2.transform
         # only converts the Image sample['img'], the other members being pass-through
         # see https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_getting_started.html
-        if 'img_mask' in self.data[index]:
-            mask_t = None
-            if self.data[index]['img_mask'].suffix == '.gz':
-                with gzip.GzipFile(self.data[index]['img_mask'], 'r') as mask_in:
-                    mask_t = torch.tensor( np.load( mask_in )/255 )
+        if 'img_channel' in self.data[index]:
+            channel_t = None
+            if self.data[index]['img_channel'].suffix == '.gz':
+                with gzip.GzipFile(self.data[index]['img_channel'], 'r') as channel_in:
+                    channel_t = torch.tensor( np.load( channel_in )/255 )
             else:
-                mask_t = torch.tensor( np.load( self.data[index]['img_mask'] )/255 )
-            sample['img_mask'] = mask_t
+                channel_t = torch.tensor( np.load( self.data[index]['img_channel'] )/255 )
+            sample['img_channel'] = channel_t
 
         sample = self.transform( sample )
         sample['id'] = Path(img_path).name
@@ -937,8 +939,8 @@ class AddChannel():
         to have the same type.
         """
         transformed_sample = sample.copy()
-        del transformed_sample['img_mask']
-        transformed_sample['img']=torch.cat( [sample['img'], sample['img_mask'][None,:,:]] )
+        del transformed_sample['img_channel']
+        transformed_sample['img']=torch.cat( [sample['img'], sample['img_channel'][None,:,:]] )
 
         return transformed_sample
 
