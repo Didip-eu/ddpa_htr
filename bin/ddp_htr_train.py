c2b2f0c6927f97062f2a65186183bbd54c5fa8e1
nicolas <nprenet@gmail.com>
2025-02-24 17:54:33 +0100

    Fixing time estimate (again).

--- bin/ddp_htr_train.py
+++ bin/ddp_htr_train.py
@@ -67,7 +67,7 @@ p = {
 def duration_estimate( iterations_past, iterations_total, current_duration ):
     time_left = time.gmtime((iterations_total - iterations_past) * current_duration)
     return ''.join([
-     '{} d '.format( time_left.tm_mday-1 ) if time_left.tm_mday > 1 else '',
+     '{} d '.format( time_left.tm_mday-1 ) if time_left.tm_mday > 0 else '',
      '{} h '.format( time_left.tm_hour ) if time_left.tm_hour > 0 else '',
      '{} mn'.format( time_left.tm_min ) ])
 
